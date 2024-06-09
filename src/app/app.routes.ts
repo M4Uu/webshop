b16dfa89fe7236f5c35a6c7b28c471e76c811e37
cruzmlathuler@gmail.com <cruzmlathulerie@gmail.com>
2024-06-09 14:51:31 -0400

    feat: Layout components, routs, home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home', // localhost:4200/
+    title: 'Home',
+    loadComponent: () => import('./modules/home/pages/home/home.component').then(m => m.HomeComponent),
+    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
