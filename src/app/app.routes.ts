84d27dd336e28aa3ba5341fe9cf2d854dcbe8a31
cruzmlathuler@gmail.com <cruzmlathulerie@gmail.com>
2025-05-03 17:42:35 -0400

    more testing...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,10 +18,10 @@ export const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
+    // canActivate: [authGuard],
     children: [
       { path: 'home',loadChildren: () => import('./modules/Auth/homelogin/homelogin.module').then(m => m.HomeloginModule) },
     ],
-    // canActivate: [authGuard]
   },
   // Admin
   {
