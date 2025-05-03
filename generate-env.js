67aa2766b71e4dc3b85001e5772d56262a00e04e
cruzmlathuler@gmail.com <cruzmlathulerie@gmail.com>
2025-05-03 16:41:36 -0400

    testing envs

--- generate-env.js
+++ generate-env.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 require('dotenv').config();
 
-const apiUrl = process.env.API_URL || 'https://webshop-backend-i8ee.onrender.com/api';
+const apiUrl = 'https://webshop-backend-i8ee.onrender.com/api';
 
 const envContent = `export const environment = {
   production: true,
