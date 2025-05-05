08b017c8e352b58863b508ebd75962bae7910e74
cruzmlathuler@gmail.com <cruzmlathulerie@gmail.com>
2025-05-04 22:59:39 -0400

    changes authServ

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import { inject, Injectable } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { UserActions } from '@app/store/actions/user.action';
 import { Store } from '@ngrx/store';
 
@@ -7,20 +8,27 @@ export class AuthService {
   private readonly SESSION_KEY = 'session_data';
   private store = inject(Store)
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
   saveSession(payload: any, status: any): void {
     const sessionData = {
       payload,
       status
     }
-    localStorage.setItem(this.SESSION_KEY, JSON.stringify(sessionData));
+    if(isPlatformBrowser(this.platformId)){
+      localStorage.setItem(this.SESSION_KEY, JSON.stringify(sessionData));
+    }
     this.store.dispatch(UserActions.loginSuccess({ payload: payload }));
   }
 
   loadSession(): void {
-    const session = localStorage.getItem(this.SESSION_KEY);
-    if (session) {
-      const data = JSON.parse(session);
-      this.store.dispatch(UserActions.rehydrateSession({ payload: data?.payload, status: data?.status }));
+    let session;
+    if(isPlatformBrowser(this.platformId)){
+      session = localStorage.getItem(this.SESSION_KEY);
+      if (session) {
+        const data = JSON.parse(session);
+        this.store.dispatch(UserActions.rehydrateSession({ payload: data?.payload, status: data?.status }));
+      }
     }
   }
 
