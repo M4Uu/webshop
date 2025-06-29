ff0ab0c0507a8c05e878b680bfa7652d1c404881
cruzmlathuler@gmail.com <cruzmlathulerie@gmail.com>
2025-06-29 01:50:09 -0400

    fix: sidebar commonmodule

--- src/app/components/features/layout/Auth/sidebar/sidebar.component.ts
+++ src/app/components/features/layout/Auth/sidebar/sidebar.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
@@ -5,6 +6,7 @@ import { RouterLink } from '@angular/router';
   selector: 'app-sidebar',
   standalone: true,
   imports: [
+    CommonModule,
     RouterLink
   ],
   templateUrl: './sidebar.component.html',
