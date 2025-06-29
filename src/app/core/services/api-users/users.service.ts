b87d3140d23ceefaf31fe813c18e2d50514ee9d9
cruzmlathuler@gmail.com <cruzmlathulerie@gmail.com>
2025-06-29 12:12:40 -0400

    wtf...

--- src/app/core/services/api-users/users.service.ts
+++ src/app/core/services/api-users/users.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { LoginInf, SessionInfo, UserInfo } from '@core/models/user.interface';
+import { UserInfo } from '@core/models/user.interface';
 import { R } from '@app/global/schema/schema.response';
 import { environment } from '@env/environment';
 
@@ -12,20 +12,20 @@ export class UsersService {
   private http = inject(HttpClient);
   private apiUrl = environment.api.apiUrlUsers;
 
-  loginUser(user: LoginInf): Observable<R> {
+  loginUser(user: any): Observable<R> {
     return this.http.post<R>(this.apiUrl.login, user, { withCredentials: true })
   }
 
-  registerUser(input: UserInfo): Observable<any> {
+  registerUser(input: any): Observable<any> {
     return this.http.post(this.apiUrl.register, input, { withCredentials: true })
   }
 
-  uploadUser(input: UserInfo): Observable<any> {
+  uploadUser(input: any): Observable<any> {
     return this.http.patch<R>(this.apiUrl.upload, input, { withCredentials: true })
   }
 
-  protectedUser(): Observable<{ status: any; payload: SessionInfo }> {
-    return this.http.get<{ status: any; payload: SessionInfo }>(
+  protectedUser(): Observable<{ status: any; payload: any }> {
+    return this.http.get<{ status: any; payload: any }>(
       this.apiUrl.protected, { withCredentials: true }
     );
   }
